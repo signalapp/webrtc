6910287eed99f6a2be842aea0db6e3d588fe8045
Jim Gustafson <jim@signal.org>
2020-10-26 19:02:03 -0700

    Update objc sdk for native interfaces with videotrack

--- sdk/objc/api/peerconnection/RTCVideoTrack.mm
+++ sdk/objc/api/peerconnection/RTCVideoTrack.mm
@@ -18,6 +18,7 @@
 
 @implementation RTC_OBJC_TYPE (RTCVideoTrack) {
   NSMutableArray *_adapters;
+  rtc::scoped_refptr<webrtc::VideoTrackInterface> _videoTrack;
 }
 
 @synthesize source = _source;
@@ -34,6 +35,7 @@
                                               source.nativeVideoSource);
   if (self = [self initWithFactory:factory nativeTrack:track type:RTCMediaStreamTrackTypeVideo]) {
     _source = source;
+    _videoTrack = track;
   }
   return self;
 }
@@ -46,6 +48,17 @@
   NSParameterAssert(nativeMediaTrack);
   NSParameterAssert(type == RTCMediaStreamTrackTypeVideo);
   if (self = [super initWithFactory:factory nativeTrack:nativeMediaTrack type:type]) {
+    _adapters = [NSMutableArray array];
+  }
+  return self;
+}
+
+- (instancetype)initWithFactory:(RTC_OBJC_TYPE(RTCPeerConnectionFactory) *)factory
+                    nativeTrack:
+                        (rtc::scoped_refptr<webrtc::MediaStreamTrackInterface>)nativeMediaTrack {
+  NSParameterAssert(factory);
+  NSParameterAssert(nativeMediaTrack);
+  if (self = [super initWithFactory:factory nativeTrack:nativeMediaTrack type:RTCMediaStreamTrackTypeVideo]) {
     _adapters = [NSMutableArray array];
   }
   return self;
@@ -102,6 +115,10 @@
       [_adapters objectAtIndex:indexToRemove];
   self.nativeVideoTrack->RemoveSink(adapterToRemove.nativeVideoRenderer);
   [_adapters removeObjectAtIndex:indexToRemove];
+}
+
+- (void *)getNativeVideoTrack {
+  return _videoTrack.release();
 }
 
 #pragma mark - Private
