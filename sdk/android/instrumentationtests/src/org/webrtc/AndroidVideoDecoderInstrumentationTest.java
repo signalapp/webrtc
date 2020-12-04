7b45e73d3322964b22e16e151edded32a9740001
Jim Gustafson <jim@signal.org>
2020-12-04 07:52:40 -0800

    Convert SDK to use androidx for annotations

--- sdk/android/instrumentationtests/src/org/webrtc/AndroidVideoDecoderInstrumentationTest.java
+++ sdk/android/instrumentationtests/src/org/webrtc/AndroidVideoDecoderInstrumentationTest.java
@@ -13,8 +13,8 @@ package org.webrtc;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import android.support.annotation.Nullable;
-import android.support.test.filters.SmallTest;
+import androidx.annotation.Nullable;
+import androidx.test.filters.SmallTest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- sdk/android/instrumentationtests/src/org/webrtc/RtpSenderTest.java
+++ sdk/android/instrumentationtests/src/org/webrtc/RtpSenderTest.java
@@ -16,8 +16,8 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
-import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.SmallTest;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.filters.SmallTest;
 import java.util.Arrays;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.junit.Before;
