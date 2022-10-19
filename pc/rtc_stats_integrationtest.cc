f54eba434b690428873c1f8dca67c3f2deb1541b
Henrik Boström <hbos@webrtc.org>
2022-10-19 17:50:09 +0200

    [Stats] Add googTimingFrameInfo to the modern API.
    This is exposing something that is already exposed in the legacy
    getStats() API and is only available if the "video-timing" header
    extension is used. Adding this metric here should unblock legacy
    getStats() API deprecation. The follow-up to unship or standardize this
    metric is tracked by https://crbug.com/webrtc/14586.
    
    Bug: webrtc:14587
    Change-Id: Ic3d45b0558d7caf4be2856a4cd95b88db312f85e
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/279860
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Commit-Queue: Henrik Boström <hbos@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#38444}

--- pc/rtc_stats_integrationtest.cc
+++ pc/rtc_stats_integrationtest.cc
@@ -945,6 +945,7 @@ class RTCStatsReportVerifier {
           inbound_stream.total_interruption_duration);
       verifier.TestMemberIsNonNegative<double>(
           inbound_stream.min_playout_delay);
+      verifier.TestMemberIsDefined(inbound_stream.goog_timing_frame_info);
     } else {
       verifier.TestMemberIsUndefined(inbound_stream.frames_decoded);
       verifier.TestMemberIsUndefined(inbound_stream.key_frames_decoded);
@@ -973,6 +974,7 @@ class RTCStatsReportVerifier {
       verifier.TestMemberIsNonNegative<double>(
           inbound_stream.total_interruption_duration);
       verifier.TestMemberIsUndefined(inbound_stream.min_playout_delay);
+      verifier.TestMemberIsUndefined(inbound_stream.goog_timing_frame_info);
     }
     return verifier.ExpectAllMembersSuccessfullyTested();
   }
