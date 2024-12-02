1f908d4441283ec069764b52cbb2ff7728741f96
Miriam Zimmerman <mutexlox@signal.org>
2024-12-02 17:00:37 -0500

    Use RE2 instead of std::regex for SDP munging.

--- ringrtc/rffi/src/sdp_observer.cc
+++ ringrtc/rffi/src/sdp_observer.cc
@@ -6,7 +6,7 @@
 #include "rffi/api/sdp_observer_intf.h"
 #include "rffi/src/ptr.h"
 #include "rffi/src/sdp_observer.h"
-#include <regex>
+#include "third_party/re2/src/re2/re2.h"
 
 namespace webrtc {
 namespace rffi {
@@ -29,8 +29,10 @@ void CreateSessionDescriptionObserverRffi::OnSuccess(SessionDescriptionInterface
   // TODO tweak the response a little
   std::string sdp;
   if (session_description->ToString(&sdp)) {
-    sdp = std::regex_replace(sdp, std::regex("(a=fmtp:111 ((?!cbr=).)*)\r?\n"), "$1;cbr=1\r\n");
-    sdp = std::regex_replace(sdp, std::regex(".+urn:ietf:params:rtp-hdrext:ssrc-audio-level.*\r?\n"), "");
+    static LazyRE2 fmtp_re = {"(a=fmtp:111 ((?!cbr=).)*)\r?\n"};
+    RE2::Replace(&sdp, *fmtp_re, "$1;cbr=1\r\n");
+    static LazyRE2 ssrc_re = {".+urn:ietf:params:rtp-hdrext:ssrc-audio-level.*\r?\n"};
+    RE2::Replace(&sdp, *ssrc_re, "");
 
     std::unique_ptr<SessionDescriptionInterface> session_description2 = CreateSessionDescription(session_description->GetType(), sdp);
     delete session_description;
