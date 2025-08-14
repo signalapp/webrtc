7d159d47a193d1048a63e6cc12cae4039853c7ff
Danil Chapovalov <danilchap@webrtc.org>
2025-08-14 18:58:05 +0200

    Save Environment in the AudioEncoderOpus
    This allows to simplify AudioEncoder api as it doesn't need to pass
    RtcEventLog that is already available for the encoder implementation.
    
    It would also would allow to rely on propagated rather than global clock.
    
    Bug: webrtc:42223992
    Change-Id: Ic42938e62938acf3cd6a11b616364809c63d69d9
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/404520
    Reviewed-by: Jakob Ivarsson‎ <jakobi@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#45348}

--- audio/audio_send_stream.cc
+++ audio/audio_send_stream.cc
@@ -605,8 +605,14 @@ bool AudioSendStream::SetupSendCodec(const Config& new_config) {
 
   // Enable ANA if configured (currently only used by Opus).
   if (new_config.audio_network_adaptor_config) {
+// TODO: bugs.webrtc.org/42223992 - call non-deprecated variant of the
+// `EnableAudioNetworkAdaptor` when deprecated one is removed from the
+// interface.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
     if (encoder->EnableAudioNetworkAdaptor(
             *new_config.audio_network_adaptor_config, &env_.event_log())) {
+#pragma clang diagnostic pop
       RTC_LOG(LS_INFO) << "Audio network adaptor enabled on SSRC "
                        << new_config.rtp.ssrc;
     } else {
@@ -706,8 +712,14 @@ void AudioSendStream::ReconfigureANA(const Config& new_config) {
   if (new_config.audio_network_adaptor_config) {
     channel_send_->CallEncoder([&](AudioEncoder* encoder) {
       RTC_DCHECK_RUN_ON(&worker_thread_checker_);
+// TODO: bugs.webrtc.org/42223992 - call non-deprecated variant of the
+// `EnableAudioNetworkAdaptor` when deprecated one is removed from the
+// interface.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
       if (encoder->EnableAudioNetworkAdaptor(
               *new_config.audio_network_adaptor_config, &env_.event_log())) {
+#pragma clang diagnostic pop
         RTC_LOG(LS_INFO) << "Audio network adaptor enabled on SSRC "
                          << new_config.rtp.ssrc;
         if (overhead_per_packet_ > 0) {
